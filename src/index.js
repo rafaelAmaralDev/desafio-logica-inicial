9c572406b0e64904f8ce93e3c8b5cc31e042fcd8
rafaelAmaralDev <rafaglhf@gmail.com>
2026-02-07 00:15:03 -0300

    agora vai

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-let xp = 1500
+let xp = 10000
 let heroi = ("Rafa")
+let nivel = ""
 
-if (xp < 1000) {
-    xp = 'Ferro'
-} else if (xp >= 1001 && xp < 2000) {
-    xp = 'Bronze'
-} else if (xp >= 2001 && xp < 5000) {
-    xp = 'Prata'
-} else if (xp >= 5001 && xp < 7000) {
-    xp = 'Ouro'
-} else if (xp >= 7001 && xp < 8000) {
-    xp = 'Platina'
-} else if (xp >= 8001 && xp < 9000) {
-    xp = 'Ascendente'
-} else if (xp >= 9001 && xp < 10000) {
-    xp = 'Imortal'
-} else if (xp >= 10001) {   
-    xp = 'Radiante'
+//usando esse for só pra cumprir com o que foi pedido, mas poderia ser feito sem ele, usando apenas if e else if.
+for (let i = 0; i < 1; i++) {
+    if (xp <= 1000) {
+    nivel = 'Ferro'
+    } else if (xp >= 1001 && xp <= 2000) {
+    nivel = 'Bronze'
+    } else if (xp >= 2001 && xp <= 5000) {
+    nivel = 'Prata'
+    } else if (xp >= 5001 && xp <= 7000) {
+    nivel = 'Ouro'
+    } else if (xp >= 7001 && xp <= 8000) {
+    nivel = 'Platina'
+    } else if (xp >= 8001 && xp <= 9000) {
+    nivel = 'Ascendente'
+    } else if (xp >= 9001 && xp <= 10000) {
+    nivel = 'Imortal'
+    } else if (xp >= 10001) {   
+    nivel = 'Radiante'
+    }
 }
 
-console.log("O Herói de nome " + heroi + " está no nível de " + xp)
+console.log("O Herói de nome " + heroi + " está no nível de " + nivel)
 
